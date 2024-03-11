274b1dca3219953e9a5f832ff053459359b8d36b
Ahmad Fathan Hanif <ahmadfathanhanif@gmail.com>
2024-03-11 19:56:03 +0700

    chore: add rate limiting

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,23 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { APP_INTERCEPTOR, APP_PIPE } from '@nestjs/core';
+import { APP_FILTER, APP_GUARD, APP_INTERCEPTOR, APP_PIPE } from '@nestjs/core';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
+import {
+  HTTP_EXCEPTION_FILTER_KEY,
+  NestProblemDetailsModule,
+} from 'nest-problem-details-filter';
 import { ZodValidationPipe } from 'nestjs-zod';
-import { EnvSchema } from '~common/types';
-import { MinioModule } from '~lib/minio/minio.module';
-import { PrismaModule } from './lib/prisma/prisma.module';
-import { V1Module } from '~v1/v1.module';
-import { V2Module } from '~v2/v2.module';
 import { CommonModule } from '~common/common.module';
 import { ResponseInterceptor } from '~common/interceptors';
+import { EnvSchema } from '~common/types';
+import { MinioModule } from '~lib/minio/minio.module';
+import { V1Module } from '~v1/v1.module';
+import { V2Module } from '~v2/v2.module';
+import { PrismaModule } from './lib/prisma/prisma.module';
 
 @Module({
   imports: [
+    NestProblemDetailsModule,
     ConfigModule.forRoot({
       isGlobal: true,
       cache: true,
@@ -25,6 +31,15 @@ import { ResponseInterceptor } from '~common/interceptors';
         return result.data;
       },
     }),
+    ThrottlerModule.forRoot({
+      throttlers: [
+        {
+          name: 'default',
+          ttl: 60000, // 60 seconds
+          limit: 150,
+        },
+      ],
+    }),
     CommonModule,
     PrismaModule,
     MinioModule,
@@ -33,12 +48,20 @@ import { ResponseInterceptor } from '~common/interceptors';
   ],
   providers: [
     {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
+    {
       provide: APP_PIPE,
       useClass: ZodValidationPipe,
     },
     {
       provide: APP_INTERCEPTOR,
       useClass: ResponseInterceptor,
+    },
+    {
+      provide: APP_FILTER,
+      useExisting: HTTP_EXCEPTION_FILTER_KEY,
     },
   ],
 })
